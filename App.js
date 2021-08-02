a4df412bedb5d498baaf66970ac09db244f86d6b
クロス尚美 <34782256+n-kanreky@users.noreply.github.com>
2021-08-02 19:42:44 +0100

    2Aug2021 in working order

--- App.js
+++ App.js
@@ -1,21 +1,126 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  View
+} from "react-native";
+import { SwipeListView } from "react-native-swipe-list-view";
 
-export default function App() {
+const Todo = props => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.todo}>
+      <Text style={styles.todoText}>{props.content}</Text>
     </View>
   );
-}
+};
+
+const App = () => {
+  const [content, setContent] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [id, setId] = useState(0);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.headerTitle}>Todoリスト</Text>
+      <TextInput
+        style={styles.todoInput}
+        onChangeText={text => setContent(text)}
+        value={content}
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          //空文字はNG
+          if (content == "") {
+            return false;
+          }
+          setId(id + 1);
+          setTodos(todos.concat([{ id: id, content: content }]));
+          setContent("");
+        }}
+      >
+        <Text style={styles.buttonText}>追加する</Text>
+      </TouchableOpacity>
+      <SwipeListView
+        data={todos}
+        renderItem={({ item }) => {
+          return <Todo content={item.content} />;
+        }}
+        keyExtractor={item => item.id.toString()}
+        renderHiddenItem={(data, rowMap) => (
+          <View style={styles.rowBack}>
+            <TouchableOpacity
+              style={{
+                alignSelf: "flex-end",
+                width: 75,
+                height: 50,
+                backgroundColor: "#CC3333"
+              }}
+              onPress={() => {
+                setTodos(
+                  todos.filter(todo => {
+                    return todo.id != data.item.id;
+                  })
+                );
+              }}
+            >
+              <Text style={{ lineHeight: 46, textAlign: "center" }}>削除</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+        rightOpenValue={-75}
+        disableRightSwipe={true}
+      />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  headerTitle: {
+    textAlign: "center",
+    fontSize: 30
   },
+  todoInput: {
+    backgroundColor: "#fff",
+    width: "70%",
+    alignSelf: "center",
+    marginTop: 20,
+    padding: 10,
+    fontSize: 20
+  },
+  container: {
+    backgroundColor: "#ddd",
+    flex: 1
+  },
+  button: {
+    backgroundColor: "blue",
+    width: "60%",
+    alignSelf: "center",
+    marginTop: 10,
+    marginBottom: 20
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+    padding: 10
+  },
+  todo: {
+    height: 50,
+    backgroundColor: "#fff",
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: "#d6d7da",
+    padding: 10
+  },
+  todoText: {
+    textAlign: "center",
+    fontSize: 20
+  }
 });
+
+export default App;
